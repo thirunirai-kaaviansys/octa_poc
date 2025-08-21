1f9d84bb4e61792f7956dff797bad465dcfd3d85
Tejash-Dhakshin-S <tejashdhakshin.s@kaaviansys.com>
2025-08-21 17:08:27 +0530

    updated workflow

--- server/index.js
+++ server/index.js
@@ -17,5 +17,5 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server is running on port 3001`);
+  console.log(`Server is running on port ${port}`);
 });

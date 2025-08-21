f64291a3521912ecea66d1e11617d9eb1faea4a3
Tejash-Dhakshin-S <tejashdhakshin.s@kaaviansys.com>
2025-08-21 16:38:57 +0530

    updated workflow

--- server/index.js
+++ server/index.js
@@ -4,19 +4,16 @@ const path = require('path');
 
 const app = express();
 
-const NODE_ENV = process.env.NODE_ENV || ['DEV'];
-
 app.use(cors());
 app.use(express.json());
 
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, './public')));
 
-if (['DEV'].includes(NODE_ENV)) {
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
 
 app.listen(3001, () => {
   console.log(`Server is running on port 3001`);
